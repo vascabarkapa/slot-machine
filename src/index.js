01a0feb6f70c171225e742b59d19e43267bde00d
vascabarkapa <vascabarkapa@gmail.com>
2024-04-13 00:27:16 +0200

    spin button

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import gamePng from './sprites/GAME.png';
 (async () => {
     const COLOR_BLACK = '#000000'
 
+    // Init App
     const app = new Application();
     await app.init({ background: COLOR_BLACK, resizeTo: window });
     document.body.appendChild(app.canvas);
 
+    // Background
     const gameAsset = await Assets.load(gamePng);
     const gameTexture = new Texture({ source: gameAsset, frame: new Rectangle(0, 0, 2048, 1024) });
 
@@ -20,6 +22,7 @@ import gamePng from './sprites/GAME.png';
     gameSprite.x = app.screen.width / 2;
     gameSprite.y = app.screen.height / 2;
 
+    // Spin button
     const obj = new Graphics()
         .roundRect(0, 0, 200, 50, 20)
         .fill('#000000');
