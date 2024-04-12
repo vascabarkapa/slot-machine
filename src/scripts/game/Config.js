ccc34789fce07c626013acf1bba80b97c708e0b0
vascabarkapa <vascabarkapa@gmail.com>
2024-04-12 12:48:26 +0200

    removed uneccessary files

--- src/scripts/game/Config.js
+++
@@ -1,7 +0,0 @@
-import { Game } from "./Game";
-import { Tools } from "../system/Tools";
-
-export const Config = {
-    loader: Tools.massiveRequire(require["context"]('./../../sprites/', true, /\.(mp3|png|jpe?g)$/)),
-    startScene: Game
-};
--- src/scripts/index.js
+++
@@ -1,4 +0,0 @@
-import { App } from "./system/App";
-import { Config } from "./game/Config";
-
-App.run(Config);
