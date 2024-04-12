690f8e69aceb5f7f4ecf2b18016353d22e9bab59
vascabarkapa <vascabarkapa@gmail.com>
2024-04-12 23:45:18 +0200

    spin text

--- src/index.js
+++ src/index.js
@@ -19,4 +19,20 @@ import gamePng from './sprites/GAME.png';
     gameSprite.anchor.set(0.5);
     gameSprite.x = app.screen.width / 2;
     gameSprite.y = app.screen.height / 2;
+
+    const spinText = new Text({
+        text: '>>> SPIN <<<',
+        style: {
+            fontFamily: 'Arial',
+            fontSize: 24,
+            fontWeight: 900,
+            fill: 'black',
+            align: 'center',
+        }
+    });
+
+    spinText.anchor.set(0.5);
+    spinText.x = app.screen.width / 2;
+    spinText.y = app.screen.height - 50;
+    app.stage.addChild(spinText);
 })();
