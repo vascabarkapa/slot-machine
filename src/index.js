042e5d568c59b0d637102762529146ace13ca3a9
vascabarkapa <vascabarkapa@gmail.com>
2024-04-13 00:29:37 +0200

    stroke button

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import { Application, Assets, Graphics, Rectangle, Sprite, Text, Texture } from 
 import gamePng from './sprites/GAME.png';
 
 (async () => {
-    const COLOR_BLACK = '#000000'
+    const COLOR_BLACK = '#000000';
+    const COLOR_ORANGE = '#FFA500';
 
     // Init App
     const app = new Application();
@@ -25,7 +26,8 @@ import gamePng from './sprites/GAME.png';
     // Spin button
     const obj = new Graphics()
         .roundRect(0, 0, 200, 50, 20)
-        .fill('#000000');
+        .fill(COLOR_BLACK)
+        .stroke({ color: COLOR_ORANGE, width: 2 });
 
     obj.x = app.screen.width / 2 - 100;
     obj.y = app.screen.height - 75;
@@ -37,7 +39,7 @@ import gamePng from './sprites/GAME.png';
             fontFamily: 'Arial',
             fontSize: 40,
             fontWeight: 900,
-            fill: 'orange',
+            fill: COLOR_ORANGE,
             align: 'center',
         }
     });
