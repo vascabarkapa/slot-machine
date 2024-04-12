3108d493848aaeebd8ef31019457f47df6b0da29
vascabarkapa <vascabarkapa@gmail.com>
2024-04-12 23:45:03 +0200

    added background

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import { Application } from "pixi.js";
+import { Application, Assets, Container, Graphics, Rectangle, Sprite, Text, TextStyle, Texture } from "pixi.js";
+
+import gamePng from './sprites/GAME.png';
 
 (async () => {
     const COLOR_BLACK = '#000000'
@@ -7,4 +9,14 @@ import { Application } from "pixi.js";
     await app.init({ background: COLOR_BLACK, resizeTo: window });
     document.body.appendChild(app.canvas);
 
+    const gameAsset = await Assets.load(gamePng);
+    const gameTexture = new Texture({ source: gameAsset, frame: new Rectangle(0, 0, 2048, 1024) });
+
+    const gameSprite = new Sprite(gameTexture);
+    gameSprite.scale = 0.75
+    app.stage.addChild(gameSprite);
+
+    gameSprite.anchor.set(0.5);
+    gameSprite.x = app.screen.width / 2;
+    gameSprite.y = app.screen.height / 2;
 })();
