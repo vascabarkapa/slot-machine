3c8c788dee13d83ca5fc584100d32975c3448e93
vascabarkapa <vascabarkapa@gmail.com>
2024-04-13 14:40:05 +0200

    loaded symbols

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import { Application, Assets, Graphics, Rectangle, Sprite, Text, Texture } from "pixi.js";
+import { Application, Assets, BlurFilter, Container, Graphics, Rectangle, Sprite, Spritesheet, Text, Texture } from "pixi.js";
 
 import gamePng from './sprites/GAME.png';
+import symPng from './sprites/SYM.png';
 
 (async () => {
+    const REEL_WIDTH = 300;
+    const SYMBOL_SIZE = 200;
     const COLOR_BLACK = '#000000';
     const COLOR_ORANGE = '#FFA500';
 
@@ -22,6 +25,209 @@ import gamePng from './sprites/GAME.png';
     gameSprite.anchor.set(0.5);
     gameSprite.x = app.screen.width / 2;
     gameSprite.y = app.screen.height / 2;
+
+    const symData = {
+        frames: {
+            P_1: {
+                frame: {
+                    x: 1,
+                    y: 1,
+                    w: 177,
+                    h: 176
+                },
+                rotated: false,
+                trimmed: true,
+                spriteSourceSize: {
+                    x: 95,
+                    y: 23,
+                    w: 177,
+                    h: 176
+                },
+                sourceSize: {
+                    w: 367,
+                    h: 222
+                }
+            },
+            P_2: {
+                frame: {
+                    x: 180,
+                    y: 1,
+                    w: 177,
+                    h: 176
+                },
+                rotated: false,
+                trimmed: true,
+                spriteSourceSize: {
+                    x: 95,
+                    y: 23,
+                    w: 177,
+                    h: 176
+                },
+                sourceSize: {
+                    w: 367,
+                    h: 222
+                }
+            },
+            P_3: {
+                frame: {
+                    x: 359,
+                    y: 1,
+                    w: 177,
+                    h: 176
+                },
+                rotated: false,
+                trimmed: true,
+                spriteSourceSize: {
+                    x: 95,
+                    y: 23,
+                    w: 177,
+                    h: 176
+                },
+                sourceSize: {
+                    w: 367,
+                    h: 222
+                }
+            },
+            P_4: {
+                frame: {
+                    x: 538,
+                    y: 1,
+                    w: 177,
+                    h: 176
+                },
+                rotated: false,
+                trimmed: true,
+                spriteSourceSize: {
+                    x: 95,
+                    y: 23,
+                    w: 177,
+                    h: 176
+                },
+                sourceSize: {
+                    w: 367,
+                    h: 222
+                }
+            },
+            P_5: {
+                frame: {
+                    x: 717,
+                    y: 1,
+                    w: 177,
+                    h: 176
+                },
+                rotated: false,
+                trimmed: true,
+                spriteSourceSize: {
+                    x: 95,
+                    y: 23,
+                    w: 177,
+                    h: 176
+                },
+                sourceSize: {
+                    w: 367,
+                    h: 222
+                }
+            },
+            P_6: {
+                frame: {
+                    x: 1,
+                    y: 179,
+                    w: 177,
+                    h: 176
+                },
+                rotated: true,
+                trimmed: true,
+                spriteSourceSize: {
+                    x: 95,
+                    y: 23,
+                    w: 177,
+                    h: 176
+                },
+                sourceSize: {
+                    w: 367,
+                    h: 222
+                }
+            },
+            P_7: {
+                frame: {
+                    x: 1,
+                    y: 358,
+                    w: 177,
+                    h: 176
+                },
+                rotated: true,
+                trimmed: true,
+                spriteSourceSize: {
+                    x: 95,
+                    y: 23,
+                    w: 177,
+                    h: 176
+                },
+                sourceSize: {
+                    w: 367,
+                    h: 222
+                }
+            },
+            P_8: {
+                frame: {
+                    x: 1,
+                    y: 537,
+                    w: 177,
+                    h: 176
+                },
+                rotated: true,
+                trimmed: true,
+                spriteSourceSize: {
+                    x: 95,
+                    y: 23,
+                    w: 177,
+                    h: 176
+                },
+                sourceSize: {
+                    w: 367,
+                    h: 222
+                }
+            },
+            P_9: {
+                frame: {
+                    x: 1,
+                    y: 716,
+                    w: 177,
+                    h: 176
+                },
+                rotated: true,
+                trimmed: true,
+                spriteSourceSize: {
+                    x: 95,
+                    y: 23,
+                    w: 177,
+                    h: 176
+                },
+                sourceSize: {
+                    w: 367,
+                    h: 222
+                }
+            }
+        },
+        meta: {
+            app: "https://www.codeandweb.com/texturepacker",
+            version: "1.0",
+            image: "SYM.png",
+            format: "RGBA8888",
+            size: {
+                w: 1024,
+                h: 1024
+            },
+            scale: "1",
+            smartupdate: "$TexturePacker:SmartUpdate:12b4a6fef4222877e3064342aaa0b152:9eba638e645aec6d7b29ed8dc87ad9ad:7f439f11c904196e4d223ad99049b174$"
+        }
+    };
+
+    const symAsset = await Assets.load(symPng);
+    const symTexture = new Texture(symAsset);
+
+    const sheet = new Spritesheet(symTexture, symData);
+    await sheet.parse();
 
     // Spin button
     const spinButton = new Graphics()
