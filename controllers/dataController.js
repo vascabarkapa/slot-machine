77eaf471df4a15ac564188d50b9c7a4d84658634
vascabarkapa <vascabarkapa@gmail.com>
2024-04-18 18:54:22 +0200

    get win symbols

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -16,6 +16,36 @@ const getReel = asyncHandler(async (req, res) => {
     }
 });
 
+/**
+ * @desc Return winning symbols
+ * @access Public
+ */
+const getWinSymbols = asyncHandler(async (req, res) => {
+    try {
+        const files = [
+            'P_1.json',
+            'P_2.json',
+            'P_3.json',
+            'P_4.json',
+            'P_5.json',
+            'P_6.json',
+            'P_7.json',
+            'P_8.json',
+            'P_9.json'
+        ];
+
+        const data = files.map((file) => {
+            const rawData = fs.readFileSync(`./data/${file}`);
+            return JSON.parse(rawData);
+        });
+
+        res.json(data);
+    } catch (error) {
+        console.error("Error: ", error);
+    }
+});
+
 module.exports = {
-    getReel
+    getReel,
+    getWinSymbols
 }
--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { getReel } = require("../controllers/dataController");
+const { getReel, getWinSymbols } = require("../controllers/dataController");
 const router = express.Router();
 
 router.route("/reel").get(getReel);
+router.route("/win").get(getWinSymbols);
 
 module.exports = router;
