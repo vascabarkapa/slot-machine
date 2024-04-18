7bd9947ce270088ffdfa00db5cf2cf5066d5c3da
vascabarkapa <vascabarkapa@gmail.com>
2024-04-19 00:49:18 +0200

    server config

---
+++ config/index.js
@@ -0,0 +1,9 @@
+import { config } from 'dotenv';
+config();
+
+const { PORT, API_URL, FRONT_URL } = process.env
+
+export const port = PORT || 5001;
+export const apiUrl = API_URL;
+export const frontUrl = FRONT_URL;
+export const prefix = '/api';
