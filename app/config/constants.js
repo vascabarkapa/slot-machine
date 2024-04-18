93870b9d33c200444a094e1806f979a332416f40
vascabarkapa <vascabarkapa@gmail.com>
2024-04-18 23:51:14 +0200

    constants

---
+++ app/config/constants.js
@@ -0,0 +1,10 @@
+// Configs
+export const API_URL = 'http://localhost:5000';
+
+// Colors
+export const COLOR_BLACK = '#000000';
+export const COLOR_ORANGE = '#FFA500';
+
+// Sizes
+export const REEL_WIDTH = 300;
+export const SYMBOL_SIZE = 170;
