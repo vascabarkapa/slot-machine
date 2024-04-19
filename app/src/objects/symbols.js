4e234b6d908d2d4ce33687ec189d26c6b8051815
vascabarkapa <vascabarkapa@gmail.com>
2024-04-19 16:50:02 +0200

    handled symbols

--- app/src/objects/symbols.js
+++ app/src/objects/symbols.js
@@ -4,18 +4,27 @@ import { API_URL } from "../../configs/constants";
 import symPng from './../../assets/sprites/SYM.png';
 
 export async function getSymbols() {
-    const symbolsResponse = await fetch(API_URL + "/reel");
-    if (!symbolsResponse.ok) {
-        throw new Error('Network response was not ok');
+    try {
+        const symbolsResponse = await fetch(API_URL + "/reel");
+        if (!symbolsResponse.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const responseSymbolsData = await symbolsResponse.json();
+        const symData = responseSymbolsData;
+
+        const symAsset = await Assets.load(symPng);
+        const symTexture = new Texture(symAsset);
+
+        const symbolsSheet = new Spritesheet(symTexture, symData);
+        await symbolsSheet.parse();
+
+        return symbolsSheet;
+    } catch (error) {
+        console.error("Error fetching symbols:", error);
+        createInformationText('Error fetching symbols! Please check your network connection and try again!', app);
+
+        setInterval(() => {
+            location.reload();
+        }, 3000);
     }
-    const responseSymbolsData = await symbolsResponse.json();
-    const symData = responseSymbolsData;
-
-    const symAsset = await Assets.load(symPng);
-    const symTexture = new Texture(symAsset);
-
-    const symbolsSheet = new Spritesheet(symTexture, symData);
-    await symbolsSheet.parse();
-
-    return symbolsSheet;
 }
